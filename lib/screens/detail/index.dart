3965874116b19de23c04fd9bcced69bf66a7b56c
Le Trung Son <lesonlhld@gmail.com>
2023-09-16 14:02:31 +0700

    💄 update detail UI

--- lib/screens/detail/index.dart
+++ lib/screens/detail/index.dart
@@ -13,35 +13,73 @@ class Detail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(blog.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Hero(
-              tag: blog.image,
-              child: Image.network(blog.image),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                children: [
-                  Row(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Hero(
+                  tag: blog.image,
+                  child: Image.network(
+                    blog.image,
+                    fit: BoxFit.cover,
+                    width: size.width,
+                    height: size.height * 0.3,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Column(
                     children: [
-                      Expanded(child: Text('Author: ${blog.authorName}')),
-                      Text(blog.date),
+                      Row(
+                        children: [
+                          Expanded(child: Text('Author: ${blog.authorName}')),
+                          Text(blog.date),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      HtmlWidget(blog.content),
+                      const SizedBox(height: 24),
                     ],
                   ),
-                  const SizedBox(height: 12),
-                  HtmlWidget(blog.content),
-                  const SizedBox(height: 24),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: kToolbarHeight,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.grey[800]!,
+                  Colors.transparent,
                 ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
             ),
-          ],
-        ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsetsDirectional.only(start: 8.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
